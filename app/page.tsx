3c8517d2d955d3d0905b056e9d47e484f59f3638
s3xmaxman <badside3919@gmail.com>
2024-05-27 15:27:16 +0900

    add Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import Image from "next/image";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FaHome } from "react-icons/fa";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="relative">
-      <div>
-        <h1>Portfolio</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+          ]}
+        />
+        <Hero />
+        <Grid />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
---
+++ components/Grid.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BentoGrid } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">{}</BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
