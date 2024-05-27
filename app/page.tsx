199d4e22f8f41704fddeaf8eb617ddc0035c5f77
s3xmaxman <badside3919@gmail.com>
2024-05-27 17:26:17 +0900

    add grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { BentoGrid } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid className="w-full py-20">{}</BentoGrid>
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
     </section>
   );
 };
