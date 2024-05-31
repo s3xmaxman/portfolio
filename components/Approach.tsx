cde84b13df83d78838f07a8e832c85d23ed3e2e1
s3xmaxman <badside3919@gmail.com>
2024-05-31 18:41:06 +0900

    update page.tsx

---
+++ components/Approach.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { CanvasRevealEffect } from "./ui/CanvasRevealEffect";
+
+const Approach = () => {
+  return <div>Approach</div>;
+};
+
+export default Approach;
